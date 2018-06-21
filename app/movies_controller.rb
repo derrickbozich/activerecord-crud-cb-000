d7c716b2112e12d4c63d310c12b9a6661dc201b3
derrick b <dbozich1@gmail.com>
2018-06-21 19:14:56 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -10,20 +10,20 @@ def can_be_instantiated_and_then_saved
   movie.title = "This is a title."
   movie.save
 end
-#
-# def can_be_created_with_a_hash_of_attributes
-#   attributes = {
-#       title: "The Sting",
-#       release_date: 1973,
-#       director: "George Roy Hill",
-#       lead: "Paul Newman",
-#       in_theaters: false
-#   }
-#   movie = Movie.new(attributes)
-#   movie.save
-#   movie
-# end
-#
+
+def can_be_created_with_a_hash_of_attributes
+  attributes = {
+      title: "The Sting",
+      release_date: 1973,
+      director: "George Roy Hill",
+      lead: "Paul Newman",
+      in_theaters: false
+  }
+  movie = Movie.new(attributes)
+  movie.save
+  movie
+end
+
 # def can_be_created_in_a_block(args = {})
 #   Movie.create do |m|
 #     m.title = args[:title]
@@ -103,18 +103,6 @@ end
 #   movie.save
 # end
 
-def can_be_created_with_a_hash_of_attributes
-  attributes = {
-      title: "The Sting",
-      release_date: 1973,
-      director: "George Roy Hill",
-      lead: "Paul Newman",
-      in_theaters: false
-  }
-  movie = Movie.new(attributes)
-  movie.save
-  movie
-end
 
 def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 })
   Movie.create do |m|
